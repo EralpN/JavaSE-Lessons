0a0bd3cb265472addb9a9af4c17d5332005948c5
EralpN <eralp.nitelik@gmail.com>
2022-04-19 14:38:10 +0300

    Fixes to PrintFunctionsPractice

--- com/bilgeadam/practice007/PrintFunctionsPractice.java
+++ com/bilgeadam/practice007/PrintFunctionsPractice.java
@@ -7,14 +7,16 @@ public class PrintFunctionsPractice
 
 	public static void main(String[] args)
 	{
+// name and surname defined
 		String name = "Eralp";
 		String surname = "Nitelik";
-
+// name and surname in a single line
 		System.out.print(name + " ");
 		System.out.println(surname);
-
-		System.out.print(name + " " + surname);
-
+// name and surname on separate lines
+		System.out.println(name);
+		System.out.print(surname);
+// warning message test
 		JOptionPane.showMessageDialog(null, "Oops. Someting went wrong.", "System Error", 0);
 	}
 
