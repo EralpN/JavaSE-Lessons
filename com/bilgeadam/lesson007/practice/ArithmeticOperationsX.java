f650400c770e884a878fdf48ee2f4437dd3b29e2
EralpN <eralp.nitelik@gmail.com>
2022-04-19 17:43:25 +0300

    Fixed an issue regarding scanner.close() function in ArithmeticOperations.

--- com/bilgeadam/lesson007/practice/ArithmeticOperationsX.java
+++ com/bilgeadam/lesson007/practice/ArithmeticOperationsX.java
@@ -1,17 +1,24 @@
 package com.bilgeadam.lesson007.practice;
 
+import java.io.FilterInputStream;
 import java.util.Scanner;
 
 public class ArithmeticOperationsX
 {
 	public double numberX()
 	{
-	double x;
-	Scanner tempX = new Scanner(System.in);
-	System.out.println("Enter the first number: ");
-	x = tempX.nextDouble();
-//	tempX.close();
-	return x;
+		double x;
+//FilterInputStream does not allow System.in to be closed. (tempX.close() function closes system.in which causes next scanner to fail.)
+		Scanner tempX = new Scanner(new FilterInputStream(System.in)
+		{
+			public void close()
+			{
+			}
+		});
+		System.out.println("Enter the first number: ");
+		x = tempX.nextDouble();
+		tempX.close();
+		return x;
 	}
-	
+
 }
